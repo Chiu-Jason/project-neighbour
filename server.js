7d32105b9002a65887ee1b65f23f03b7f0f13e80
Jason Chiu <77141190+Chiu-Jason@users.noreply.github.com>
2021-04-04 02:10:09 -0700

    added error handling

--- server.js
+++ server.js
@@ -7,6 +7,17 @@ const port = 3000
 app.set('view engine', 'pug')
 app.use(express.static('public'))
 
+const multer = require('multer')
+const storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, 'public/image/product')
+    },
+    filename: function(req, file, cb){
+        cb(null, req.body.name.toLowerCase() + '.jpeg')
+    }
+})
+const upload = multer({storage: storage})
+
 const mongoose = require('mongoose')
 const mongoDB = 'mongodb://127.0.0.1:27017/neighbour'
 mongoose.connect(mongoDB, { useNewUrlParser: true});
@@ -27,7 +38,6 @@ const cart = []
 
 app.get('/', async (req, res) => {
     const featuredProducts = await productModel.aggregate([{ $sample: { size: 3 } }])
-    console.log('featured products:', featuredProducts)
     res.render('index', { featuredProducts: featuredProducts, cart});
 });
 app.get('/sell', (req, res)=>{
@@ -35,29 +45,41 @@ app.get('/sell', (req, res)=>{
 });
 app.get('/products/', async (req, res)=>{
     const products = await productModel.find().exec();
-    console.log("documents:", products)
     res.render('products', { products: products, cart})
 });
-app.get('/products/:id', async (req, res) => {
+app.get('/products/:id', async (req, res, next) => {
     const selectedId = req.params.id
     const product = await productModel.findOne({ "_id": selectedId });
-    console.log(product)
+    if (!product){
+        next('err')
+    }
     res.render('details', { product: product, cart })
 })
-app.get('/products/city/:city', async (req, res)=>{
+app.get('/products/city/:city', async (req, res, next)=>{
     const selectedCity = req.params.city
     let selectedProducts = await productModel.find({"city": selectedCity}).exec();
-    console.log(selectedProducts)
+    if (!selectedProducts){
+        next('err')
+    }
     res.render('products', { products: selectedProducts, cart });
 });
-app.get('/products/category/:category', async (req, res)=>{
+app.get('/products/category/:category', async (req, res, next)=>{
     const selectedCategory = req.params.category
     let selectedProducts = await productModel.find({"category": selectedCategory})
-    console.log(selectedProducts)
+    if (!selectedProducts){
+        next('err')
+    }
     res.render('products', { products: selectedProducts, cart });
 });
-app.post('/sell', urlencodedParser, async (req, res) => {
-    const newItem = req.body
+app.post('/sell', upload.single('image'), async (req, res) => {
+    const newItem = {
+        city: req.body.city,
+        category: req.body.category,
+        name: req.body.name,
+        price: req.body.price,
+        description: req.body.description,
+        image: req.file.filename
+    }
     await productModel.create(newItem)
     res.redirect('/products');
 })
@@ -69,18 +91,28 @@ app.get('/cart/', (req, res)=>{
     parseFloat(total).toFixed(2);
     res.render('cart', { cart: cart, subtotal, tax, total})
 })
-app.get('/cart/:id', async (req, res) => {
+app.get('/cart/:id', async (req, res, next) => {
     const selectedId = req.params.id
     const item = await productModel.findOne({"_id": selectedId }).exec();
+    if (!item){
+        next('err')
+    }
     cart.push(item); 
-    console.log(cart)
     res.redirect('/products');
 })
-app.get('/cart/delete/:id', (req, res)=>{
+app.get('/cart/delete/:id', (req, res, next)=>{
     const itemIndex = cart.findIndex(p => p.id == req.params.id)
     cart.splice(itemIndex, 1);
     res.redirect('/cart/');
 })
+app.use(function(req, res, next){
+    const err = new Error('Not Found');
+    err.status = 404;
+    next('err');
+});
+app.use(function(err, req, res, next){
+    res.status(404 || 500).render('error',{cart});
+});
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`)
 })

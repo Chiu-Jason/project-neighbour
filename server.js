ba5c6a8ac53f4bb7c51b69a71f394941f5696ff5
Jason Chiu <77141190+Chiu-Jason@users.noreply.github.com>
2021-04-16 00:05:28 -0700

    edited port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
-const port = process.env.Port || 3000
+const port = process.env.PORT || 3000
 global.cart = []
 
 app.set('view engine', 'pug')
